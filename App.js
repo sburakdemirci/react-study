d027c6ee2c5c131dd3bb4fb15a50e01c0ba5200a
burak <gfb.salih95@gmail.com>
2019-08-07 23:06:30 +0300

    fixed

--- App.js
+++ App.js
@@ -1,9 +1,8 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
-import Header from './src/components/header';
-import AlbumListFunctionalComponent from './src/components/AlbumListFunctionalComponent';
-import AlbumListClassComponent from './src/components/AlbumListClassComponent';
-
+import React, { Component } from "react";
+import { AppRegistry, Text, View } from "react-native";
+import Header from "./src/components/header";
+import AlbumListFunctionalComponent from "./src/components/AlbumListFunctionalComponent";
+import AlbumListClassComponent from "./src/components/AlbumListClassComponent";
 
 // ./ yazarak şuanki dosya konumundan başlarız.
 // import ederken AlbumList.js yazmamıza gerek yok.Dosya adı yeterli. uzantıya gerek yok
@@ -12,14 +11,13 @@ export default class LotsOfGreetings extends Component {
   render() {
     return (
       <View>
-            <Header headerText={'Albumsss'}/>
-       
-            <AlbumListClassComponent/>
-          
+        <Header headerText={"Albumsss"} />
+
+        <AlbumListClassComponent />
       </View>
     );
   }
 }
 
 // skip this line if using Create React Native App
-AppRegistry.registerComponent('learn', () => LotsOfGreetings);
+AppRegistry.registerComponent("learn", () => LotsOfGreetings);
